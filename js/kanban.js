5903a818da2e2b4e23b20105098963fc23f1cc21
chamoon-code <118274935+chamoon-code@users.noreply.github.com>
2023-02-17 11:38:24 +0800

    Update kanban.js

--- js/kanban.js
+++ js/kanban.js
@@ -20,8 +20,8 @@ const create_item = () => {
   item.classList.add('item');
   item.id = 'item-' + order;
   item.draggable = true;
-  item.addEventListener('dragstart', event => event.dataTranster.setData('text', event.target.id));
-  item.addEventListener('dragend', event => event.dataTranster.clearData());
+  item.addEventListener('dragstart', event => event.dataTransfer.setData('text', event.target.id));
+  item.addEventListener('dragend', event => event.dataTransfer.clearData());
 
   let input = document.createElement('input');
   item.appendChild(input);
@@ -46,7 +46,7 @@ const create_item = () => {
 document.querySelectorAll('.drop').forEach(element => {
   element.addEventListener('drop', event => {
     event.preventDefault();
-    const id = event.dataTranster.getData('text');
+    const id = event.dataTransfer.getData('text');
     event.target.appendChild(document.getElementById(id));
   });
   element.addEventListener('dragover', event => event.preventDefault());
